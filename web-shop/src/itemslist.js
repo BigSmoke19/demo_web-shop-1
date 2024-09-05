126618c8b75fb99910b68f624165fe155e7bf2c3
BigSmoke19 <mohammadsafieddine789@gmail.com>
2024-09-05 22:33:50 +0300

    basic item styling

--- web-shop/src/itemslist.js
+++ web-shop/src/itemslist.js
@@ -1,30 +1,25 @@
-import { Link, useNavigate } from "react-router-dom";
+import './styles/general/itemslist.css';
+import { Link} from "react-router-dom";
 
 const ItemsList = (props) => {
     const items = props.items;
     const title = props.title;
-    const history = useNavigate();
     const url = "http://localhost:8000/items/";
 
-    const handleDelete = (id) =>{
-      fetch(url + id,{method:"DELETE"})
-      .then(()=>history("/"));
-    };
     return ( 
     <div className="items">
         <h2>{title}</h2>
         {Array.from(items).map((item) => (
 
-          <div className="items-preview" key={item.id}>
+          <div className="item" key={item.id}>
             <Link className="links" to={`/items/${item.id}`}>
-            <img 
+            <img className='item-image'
               src = {`data:image/jpeg;base64,${item.image}`} 
               alt="Decoded-pic">
               </img>
               <h3>{item.name}</h3>
               <h3>{item.type}</h3>
               <h3>{item.price}$</h3>
-              <button id="delete-btn" onClick={() => handleDelete(item.id)}>Delete</button>
             </Link>
           </div>
         ))}
