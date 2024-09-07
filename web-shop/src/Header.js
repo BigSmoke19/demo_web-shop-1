12ae1f319af2babf6aa20baeea127fb908cbae17
mohammad khalil <mohammadkhalil123>
2024-09-07 23:28:23 +0300

    resize header and create sidebar

--- web-shop/src/Header.js
+++ web-shop/src/Header.js
@@ -1,9 +1,24 @@
 import './styles/home/header.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [isMenu,setIsMenu]=React.useState(false);
+
     return(
         <header>
-            <div className="left-section">
+            <div className="left-section" >
+                <div className={isMenu?"side-bar open":"side-bar"}>
+                    <Link className="link" to="/">Home</Link>
+                    <Link className="link" to="Create">Create</Link>
+                    <Link className="link" to="SignUp">SignUp</Link>
+                    <button className="cancel-button" onClick={()=>setIsMenu(false)}>
+                        X
+                    </button>
+                </div>
+                <button className="menu-icon-button" onClick={()=>setIsMenu(true)}>
+                    <img src="/images/icons/menu-icon.png" className="menu-icon" />
+                </button>
                 <p className="title">
                     Web-Shop
                 </p>
@@ -17,6 +32,9 @@ const Header = () => {
             </div>
 
             <div className="right-section">
+                <button className="white-right-section-buttons">
+                    <img src="/images/icons/white-search-icon.png" className="white-search-icon" />
+                </button>
                 <button className="right-section-buttons">
                     <img src="/images/icons/user-icon.png" className="user-icon" />
                 </button>
