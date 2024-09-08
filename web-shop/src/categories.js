7311d802aab15032c8abb7c9b646d09b3e727211
BigSmoke19 <mohammadsafieddine789@gmail.com>
2024-09-08 16:04:01 +0300

    managed login/sign up pages

--- web-shop/src/categories.js
+++ web-shop/src/categories.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import './styles/home/navbar.css'
-
+import { useCookies } from 'react-cookie';
 const Categories = () => {
+    const [cookies] = useCookies(['email']);
     return (  
         <nav>
             <Link className="link" to="/">Home</Link>
             <Link className="link" to="Create">Create</Link>
-            <Link className="link" to="SignUp">SignUp</Link>
+            {!cookies.email && <Link className="link" to="LogIn">Log In</Link>}
+            {cookies.email && <p className="link">{cookies.email}</p>}
         </nav>
     );
 }
--- web-shop/src/home.js
+++ web-shop/src/home.js
@@ -1,11 +1,10 @@
-
 import ItemsList from './itemslist';
 import useFetch from './usefetch';
 const Home = () => {
     const url = "http://localhost/webshop-apis/getdata.php";
     //const url2 = "http://localhost:8000/items";
     const {items,isPending,error} = (useFetch(url));
-
+    
     return ( 
         <div className="home">
           {error && <div>{error}</div>}     
