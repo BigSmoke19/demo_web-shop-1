57bc480c45d80ae4a06d9dded9943c7c25df74ca
mohammad khalil <mohammadkhalil123>
2024-09-08 17:43:40 +0300

    edit header and navbar

--- web-shop/src/categories.js
+++ web-shop/src/categories.js
@@ -4,9 +4,11 @@ import './styles/home/navbar.css'
 const Categories = () => {
     return (  
         <nav>
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="Create">Create</Link>
-            <Link className="link" to="SignUp">SignUp</Link>
+            <div className="categories">
+                <Link className="link" to="/">Home</Link>
+                <Link className="link" to="Create">Create</Link>
+                <Link className="link" to="SignUp">SignUp</Link>
+            </div>
         </nav>
     );
 }
