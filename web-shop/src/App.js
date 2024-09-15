e0bb75b56fde2fbc4689f430279fdad383658686
BigSmoke19 <mohammadsafieddine789@gmail.com>
2024-09-15 12:33:54 +0300

    managed admin login

--- web-shop/src/App.js
+++ web-shop/src/App.js
@@ -6,6 +6,7 @@ import ItemDetails from './itemdetails';
 import SignUp from './signup';
 import LogIn from './login';
 import Cart from './cart';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
@@ -13,12 +14,20 @@ function App() {
       <div className="App">
         <div className="content">
           <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/create" element={<Create />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/login/signup" element={<SignUp />} />
-          <Route path="/items/:id" element={<ItemDetails />} />
-          <Route path="/cart" element={<Cart />} />
+            <Route exact path="/" element={<Home />} />
+            <Route
+            path="/create"
+              element={
+                <ProtectedRoute>
+                  <Create />
+                </ProtectedRoute>
+              }
+            />
+            <Route exact path="/create" element={<Create />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/login/signup" element={<SignUp />} />
+            <Route path="/items/:id" element={<ItemDetails />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
       </div>
