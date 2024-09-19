580093002bb31f31b555c9184abf0bb8deda279a
mohammad khalil <mohammadkhalil123>
2024-09-19 03:13:48 +0300

    display items in cart side bar

--- web-shop/src/itemslist.js
+++ web-shop/src/itemslist.js
@@ -2,6 +2,7 @@ import React from "react";
 import './styles/home/listItems.css';
 
 const ItemsList = (props) => {
+
     const items = props.items;
     const title = props.title;
     const url = "http://localhost:8000/items/";
@@ -10,6 +11,8 @@ const ItemsList = (props) => {
     const [likes,setLikes]=React.useState([]);
     const [dislike,setDisLike]=React.useState(false)
     const [isAdded,setIsAdded]=React.useState(false)
+    const [quantities,setQuantities]=React.useState(Array.from(items).map(item=>({id:item.id,quantity:1})))
+    const [displayQuantity,setDisplayQuantity]=React.useState(0)
     const isHome = props.isHome;
 
     function handleMouseEnter(id) {
@@ -37,21 +40,14 @@ const ItemsList = (props) => {
     const handleCart = (e,item) =>{
       if(JSON.parse(localStorage.getItem('items')) !== null) {
           const add=e.target.value
-          let total=0
           if(add === "Add To Cart" && !isAdded) {
             const cart = JSON.parse(localStorage.getItem('items'))
             cart.push(item)
             localStorage.setItem('items',JSON.stringify(cart))
             JSON.parse(localStorage.getItem('items')).forEach(item=>{
-              total+=item.price
             })
-            console.log(total)
-            localStorage.setItem('total',total)
-            localStorage.setItem('quantity',JSON.parse(localStorage.getItem('items')).length)
           }
       } else{
-        localStorage.setItem('total',item.price)
-        localStorage.setItem('quantity',1)
         localStorage.setItem('items',JSON.stringify([item]));
       }
       setIsAdded(true)
@@ -75,9 +71,48 @@ const ItemsList = (props) => {
         <div>
            <h2>{title}</h2>
            <div className={!isAdded?"cart-side-bar":"cart-side-bar open-cart-side-bar"}>
+              <h1 style={{color:"white"}}>My Cart</h1>
               <button className="cancel-cart-bar" onClick={()=>setIsAdded(false)}>x</button>
-              <h1 style={{color:"white"}}>{localStorage.getItem('total')}</h1>
-              <h1 style={{color:"white"}}>{localStorage.getItem('quantity')}</h1>
+              <div className="cart-side-bar-items">
+                  {Array.from(JSON.parse(localStorage.getItem('items'))).map(item=>(
+                    <div className="cart-side-bar-item" key={item.id}>
+                      <div style={{backgroundColor:"lightblue"}}>
+                        <img src={`data:image/jpeg;base64,${item.image}`} className="cart-side-bar-item-image" />
+                      </div>
+                      <div className="inner-container" style={{backgroundColor:"lightpink"}}>
+                        <div className="cart-side-bar-item-inner">
+                          <p>{item.name}</p>
+                          <p>x</p>
+                        </div>
+                        <div className="cart-side-bar-item-inner">
+                          <div className="bottom-inner-container">
+                            <button className="plus-button">+</button>
+                            <button>{quantities.forEach((quantity)=>{
+                              if(quantity.id === item.id) {
+                                setDisplayQuantity(quantity.quantity)
+                                return
+                              }
+                            })}{displayQuantity}</button>
+                            <button className="minus-button">-</button>
+                            <p style={{padding:"0",margin:"0"}}>{item.price}</p>
+                          </div>
+                          <div>
+                            <p>price</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                  <div className="cart-sidebar-bottom">
+                    <button>
+                      View Cart
+                    </button>
+                    <button>
+                      Checkout
+                    </button>
+                    <p>total:</p>
+                  </div>
+              </div>
            </div>
 \          <div className="display-items">
               {Array.from(items).map((item) => (
